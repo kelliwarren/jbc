b76fd06d51b826d4eeecc55be8a220801e506f82
kelliwarren <kmdesigngroup@gmail.com>
2014-02-25 12:35:59 -0800

    made recipe and chef views and controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,49 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:success] = "New recipe has been created"
+      redirect_to recipe_path(@recipe.id)
+    else
+      flash[:error] = "There was an error saving your recipe"
+      render :new
+    end
+
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update_attributes(recipe_params)
+    flash[:notice] = "#{@recipe.title} was updated"
+    redirect_to recipe_path(@recipe.id)
+    else
+    flash[:error] = "There was an error updating your form"
+    render :edit
+    end
+
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :cook_time, :instructions, :picture, :ingredients)
+  end
 end
